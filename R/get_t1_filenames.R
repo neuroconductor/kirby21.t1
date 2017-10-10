39f96f4bc9f6cfb8759e7d6af94b4e4f2591b689
muschellij2 <muschellij2@gmail.com>
2017-10-10 12:03:56 -0400

    Fixing that typo.

--- R/get_t1_filenames.R
+++ R/get_t1_filenames.R
@@ -3,7 +3,7 @@
 #' @description Return the filenames for the T1 images
 #' @param ... arguments to pass to \code{\link{get_image_filenames}},
 #' \code{modalities = "T1"} so it cannot be specified
-#' @return Vector fo filenames
+#' @return Vector of filenames
 #' 
 #' @examples
 #' get_t1_filenames()
